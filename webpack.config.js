df8e09d60e7cc9a8040e13435e499ad774616e34
ppiotr <ppiotrowski927@gmail.com>
2017-10-07 23:20:37 +0200

    UglifyJS plugin was added

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
-
+const webpack = require('webpack');
 module.exports = {
   entry: './src/script.js', 	
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+},
+plugins: [
+  new webpack.optimize.UglifyJsPlugin({
+    minimize: true
+  })
+],
+stats: {
+    colors: true
+},
+devtool: 'source-map'
 };
